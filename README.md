2d08a1b542b070ce6ad9cabf04441ebd28b2f3d2
Craig <indigomx9@gmail.com>
2022-03-30 15:54:53 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shopping Cart
+
+Initial setup of the project.  Created with TypeScript, React 18, React Router 6, and Redux-Toolkit.
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi fugiat tempore perspiciatis aliquam maxime maiores a quisquam temporibus beatae? Perspiciatis iste minima ipsam dolore tenetur quam quos delectus reprehenderit vero.</p>
+        </React.Fragment>
+    );
+};
+
+
